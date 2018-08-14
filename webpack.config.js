c7159b8db9ad37d2fb4b54f616c1650b26ee1c53
Berkmann18 <maxieberkmann@gmail.com>
2018-08-14 15:33:26 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,99 @@
+var path = require('path')
+var webpack = require('webpack')
+var PrerenderSpaPlugin = require('prerender-spa-plugin')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+var VueLoaderPlugin = require('vue-loader')
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/main.js'),
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [{
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ],
+      }, {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {}
+          // other vue-loader options go here
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true,
+    overlay: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    mode: "production",
+    devtool: '#source-map',
+    optimization: {
+      minimizer: [
+        new UglifyJsPlugin({
+          sourceMap: true
+        })
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'index.html',
+        filename: path.resolve(__dirname, 'dist/index.html')
+      }),
+      new PrerenderSpaPlugin({
+        // Absolute path to compiled SPA
+        staticDir: path.resolve(__dirname, './dist'),
+        // List of routes to prerender
+        routes: ['/', '/about', '/contact'],
+        // Options
+        postProcess(context) {
+          let titles = {
+            '/': 'My home page',
+            '/about': 'My awesome about page',
+            '/contact': 'Contact me'
+          };
+          context.html = context.html.replace(
+            /<title>[^<]*<\/title>/i,
+            `<title>${titles[context.route]}</title>`
+          )
+          return context
+        }
+      }),
+      new VueLoaderPlugin()
+    ]
+  }
+}
